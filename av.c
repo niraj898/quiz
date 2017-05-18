a1168708e685cb7dc37c49378fc23b747f4f673b
Tiwary Niraj <niraj.cool.hot@gmail.com>
2017-05-18 20:34:14 +0530

    Add files via upload

---
+++ av.c
@@ -0,0 +1,432 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+int ch,count=0;
+int main()
+{
+    int c,ct,t,p=143;
+    system("COLOR f4");
+    printf("\n\n\n\n\n\n\n\n\n\n\n                           password plese!!!!!!!\n                              ");
+    scanf("%d",&t);
+    if(t==p)
+    {
+    }
+    else{
+        return;
+    }
+    system("cls");
+    system("COLOR 9F");
+	printf("\n\n\n\n\n\n\n\n\n\n\n\t            ***Welcome to the world of Ask-Vangers***\n\n\n\n\n\n\n\n");
+	system("pause");
+	system("cls");
+	system("COLOR 76");
+    printf("\n\n\n\n\n\n\n\n\n\n\n\t        The World has seen many....try your luck..\n\n\n");
+    system("pause");
+    system("cls");
+    system("COLOR f4");
+    printf("\n\n\n\n\n\n\n\n\t           *****Plese Select Which Ask-Vanger Do Want To Be***** \n\n\t1. Caption america\n\n\t2.Iron-Man\n\n\t3.Spidy\n\n\t \t           *OR*\n\n\t4.To See The Credits\n\n\tYour choice is => ");
+    scanf("%d",&c);
+    system("pause");
+	system("cls");
+	system("COLOR 1e");
+    printf("Enter the right option\n");
+    switch (c)
+    {
+    case 1:
+        printf("***What is Captain America's real name?\nA)Steve Rogers\nB)Bill Smith\nC)John Reck\nD)Matt Robert  ");
+  scanf("%d",&ch);
+  printf("\n");
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR Fd");
+  printf("\n***What is a Captain America's weapon?\nA)A bow\nB)A shield and fists\nC)A rock\nD)Lazer eyes");
+  scanf("%d",&ch);
+  if(ch==2)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR Fc");
+  printf("\n***What is Captain America's Villan?\nA)The Joker\nB)Red Skull\nC)The Hash Slinging Slasher\nD)lex luthor");
+  scanf("%d",&ch);
+  if(ch==2)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR Fb");
+  printf("\n***What is Captain America's home town?\nA)San Diago\nB)Las Vegas\nC)Denver\nD)Manhatten");
+  scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR Fa");
+  printf("\n***What war did Captain America fight in?\nA)Vietnam\nB)WW1\nC)Gulf war\nD)WW2");
+  scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR F9");
+  printf("\n***What is Captain America's actual team name?\nA)The Flamming Atoms\nB)The West Coast Avengers\nC)The Avengers\nD)Thee ALL-Winners Squad");
+  scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR F8");
+  printf("\n***How did Captain America become strong?\nA)Expiremental Serum\nB)He was born that way\nC)It was given to him\nD)Its in his costume");
+  scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR F9");
+  printf("\n***What is Captain America's weakness?\nA)Everything a human has\nB)Kryptonite\nC)Garlic\nD)Nothing");
+  scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR Fa");
+  printf("\n***What is the personality of Captain America?\nA)A Coward\nB)A Patriot\nC)An Oportunist\nD)A Fool");
+  scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR Fb");
+  printf("\n***Who played as Captain America in the first Captain America made for TV movie?\nA)Harrison Ford\nB)Chuck Norris\nC)Reb Brow\nD)atthew Novack");
+  scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  system("pause");
+  system("cls");
+  system("COLOR Fc");
+        break;
+    case 2:
+        system("COLOR c0");
+    printf("\n1 Who is Iron Man?\n\n1.Anthony Edward Stark\n2.Tony Howard Stark\n3.Mr. Big Sexy Man\n");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	printf("\n2 How did Iron Man become Iron Man?\n1.By Terrorist attack\n2.By traveling time\n3.By sex");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	printf("\n3 What Villain is Obadiah Stane?\n1.Titanium Man\n2.Fing Fang Foom\n3.Iron Monger");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	printf("\n4 Who Is Iron man's secretary?\n1.Virginia Potts\n2.Pepper Botts\n3.Mary Jane Watson");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	printf("\n5 Is Iron Man Dead?\n1.Yes\n2.No3.Maybe");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	printf("\n6 Who helped Iron Man escape captivity?\n1.Tio Minosin\n2.Ho Yinsen\n3.Bin Yinsen");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	printf("\n7 What color(s) is the first iron man armor?\n1.Grey\n2.Gold\n3.Red and gold");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	printf("\n8 In the film, What piece of Stark Technology was used as the mark 3 armor?\n1.Satellite\n2.Jericho bomb\n3.Car");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+
+	scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  printf("\n9 Who wrote the song Iron Man? I just threw that one in for an extra question?\n1.Iron Maiden\n2.Aerosmith\n3.Black Sabbath");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	printf("\n10 Who is Iron Man's iron suited partner?\n1.War Machine\n2.Obadiah Stane\n3.Unicorn");
+    scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+	    break;
+    case 3:
+        system("COLOR c1");
+        printf("\n1.  What year was the first Spider-Man comic released?\nA.1961\nB.1962\nC.1963\nD.1964");
+        scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  printf("\n3.  Who was the first hero(es) to help Spider-Man?\nA.Daredevil\nB.Fantastic Four\nC.Punisher\nD.Avengers");
+   scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+printf("\n4.  Who was the first villian to defeat Spider-Man?\nA.Chameleon\nB.Vulture\nC.Doctor Octopus\nD.Kingpin");
+ scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+printf("\n5.  Who was the black symbiotes first host?\nA.Peter Parker\nB.Eddie Brock\nC.Cletus Kassady\nD.John Jameson");
+ scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+printf("\n9.  Who is Peter Parker's first serious girlfriend?\nA.Mary Jane\nB.Gwen Stacy\nC.Liz Allen\nD.Alicia Hardy");
+ scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+printf("\n10.  Which villian was NOT at once Spidey's friend?\nA.New Goblin\nB.Venom\nC.Lizard\nD.Sandman");
+ scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+printf("\n10. Which heroe's first apperance was in a Spider-Man comic?\nA.Daredevil\nB.Blade\nC.Punisher\nD.HulK");
+ scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+printf("\n17.  Who played Spider-Man?\nA.Tobey Maguire\nB.James Franco");
+ scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+printf("\n19.  Who created Spider-Man?\nA.Stan Young\nB.Danny Lee\nC.Stan Lee\nD.George Lopez");
+ scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+printf("\n22.  Which of theese versions of spidey is NOT Peter Parker?\nA.Black Spider-Man\nB.Iron Spider-man\nC.Spider-Man 2022\nD.Captain Universe");
+ scanf("%d",&ch);
+  if(ch==1)
+  {
+      printf("\nyeh! Right");
+      count++;
+  }
+  else
+  {
+      printf("\nNo!!!!!");
+  }
+  break;
+    case 4:
+        system("COLOR f4");
+        printf("\n\n\n                               Devloped By\n                    Niraj Tiwary,Keyur Vahani,Milan Vaghani\n\n                               Designed By\n                                Niraj Tiwary\n\n                               produced By\n                                Niraj Tiwary\n\n                               Idea By\n                                Niraj Tiwary\n\n                               AND EVEN Co-Produced By\n                                Niraj Tiwary\n");
+        system("pause");
+        break;
+    /*
+    case 6:
+        void mix();
+        break;*/
+    default:printf("wrong choise");
+    }
+    system("cls");
+    system("COLOR f4");
+    printf("\n\n\n\n\n\n\n\n\n\n\n                          ******Congrats your score is %d******\n",count);
+    system("pause");
+    count=0;
+    return 0;
+}
